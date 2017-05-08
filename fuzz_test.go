0126a2affb8c738cf8b0822f3e79cb041dbe18f4
Thomas Desrosiers <thomas@databricks.com>
2017-05-08 03:49:50 -0700

    map: initialSize overflow

--- fuzz_test.go
+++ fuzz_test.go
@@ -40,6 +40,75 @@ func TestCrashers_OCFReader(t *testing.T) {
 
 func TestCrashers_OCF_e2e(t *testing.T) {
 	var crashers = map[string]string{
+		"map: initialSize overflow": "Obj\x01\x04\x14avro.codec\bnul" +
+			"l\x16avro.schema\xa2\x0e{\"typ" +
+			"e\":\"record\",\"name\":\"" +
+			"test_schema\",\"fields" +
+			"\":[{\"name\":\"string\"," +
+			"\"type\":\"string\",\"doc" +
+			"\":\"Meaningless strin" +
+			"g of characters\"},{\"" +
+			"name\":\"simple_map\",\"" +
+			"type\":{\"type\":\"map\"," +
+			"\"values\":\"int\"}},{\"n" +
+			"ame\":\"complex_map\",\"" +
+			"type\":{\"type\":\"map\"," +
+			"\"values\":{\"type\":\"ma" +
+			"p\",\"values\":\"string\"" +
+			"}}},{\"name\":\"union_s" +
+			"tring_null\",\"type\":[" +
+			"\"null\",\"string\"]},{\"" +
+			"name\":\"union_int_lon" +
+			"g_null\",\"type\":[\"int" +
+			"\",\"long\",\"null\"]},{\"" +
+			"name\":\"union_float_d" +
+			"ouble\",\"type\":[\"floa" +
+			"t\",\"double\"]},{\"name" +
+			"\":\"fixed3\",\"type\":{\"" +
+			"type\":\"fixed\",\"name\"" +
+			":\"fixed3\",\"size\":3}}" +
+			",{\"name\":\"fixed2\",\"t" +
+			"ype\":{\"type\":\"fixed\"" +
+			",\"name\":\"fixed2\",\"si" +
+			"ze\":2}},{\"name\":\"enu" +
+			"m\",\"type\":{\"type\":\"e" +
+			"num\",\"name\":\"Suit\",\"" +
+			"symbols\":[\"SPADES\",\"" +
+			"HEARTS\",\"DIAMONDS\",\"" +
+			"CLUBS\"]}},{\"name\":\"r" +
+			"ecord\",\"type\":{\"type" +
+			"\":\"record\",\"name\":\"r" +
+			"ecord\",\"fields\":[{\"n" +
+			"ame\":\"value_field\",\"" +
+			"type\":\"string\"}],\"al" +
+			"iases\":[\"Reco\x9adAlias" +
+			"\"]}},{\"name\":\"array_" +
+			"of_boolean\",\"type\":{" +
+			"\"type\":\"array\",\"item" +
+			"s\":\"boolean\"}},{\"nam" +
+			"e\":\"bytes\",\"type\":\"b" +
+			"ytes\"}]}\x00\xfeJ\x17\u007f\xb4r\x11\x0e\x96&\x0e" +
+			"\xda<\xed\x86\xf6\x06\xfa\x05(OMG SPARK I" +
+			"S AWESOME\x04\x06abc\x02\x06bcd\x0e" +
+			"\x00\x02\x06key\x03\x80\x00\x02d\x02a\x02b\x00\x00\x01\x00\x00" +
+			"\x00\x00\x00\x04\x00\xdb\x0fI@\x02\x03\x04\x11\x12\x00\xb6\x01Two" +
+			" things are infinite" +
+			": the universe and h" +
+			"uman stupidity; and " +
+			"I'm not sure about u" +
+			"niverse.\x06\x01\x00\x00\x00\x06ABCT\x00e" +
+			"rran is IMBA!\x04\x06qqq\x84\x01" +
+			"\x06mmm\x00\x00\x02\x06key\x04\x023\x024\x021\x02K" +
+			"��~\x02\x84\x01\x02`\xaa\xaa\xaa\xaa\xaa\x1a@\a" +
+			"\a\a\x01\x02\x06\x9e\x01Life did no\xef\xbf" +
+			"\xbd\ttend to make us pe" +
+			"rfect. Whoever is pe" +
+			"rfect `elongs in a m" +
+			"useum.\x00\x00$The cake is" +
+			" a LIE!\x00\x02\x06key\x00\x00\x00\x04\x02\x00\x00" +
+			"\x00\x00\x00\x00\x00\x00\x11\"\t\x10\x90\x04\x16TEST_ST" +
+			"R123\x00\x04\x00\x02S\xfeJ\x17\u007f\xb4r\x11\x0e\x96&\x0e" +
+			"\xda<\xed\x86\xf6",
 		"array: initialSize overflow": "Obj\x01\x04\x14avro.codec\bnul" +
 			"l\x16avro.schema\xa2\x0e{\"typ" +
 			"e\":\"record\",\"name\":\"" +
